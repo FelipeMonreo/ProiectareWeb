7ff964f3011d96863e65c4b9e67fe10e129b3fe7
Tanase Filip Andrei <filip.tanase@student.unitbv.ro>
2026-02-25 16:54:16 +0200

    feat: adauga salut personalizat bazat pe ora zilei

--- js/contact_form.js
+++ js/contact_form.js
@@ -8,3 +8,19 @@ function submitForm() {
 
     console.warn("Goodbye World!");
 }
+window.addEventListener("DOMContentLoaded", function () {
+
+  const paragraf = document.querySelector("header p");
+  const ora = new Date().getHours();
+
+  if (ora >= 6 && ora <= 11) {
+    paragraf.textContent = "Dimineata superba, pofta la cafeluta suflet drag!";
+  } 
+  else if (ora >= 12 && ora <= 16) {
+    paragraf.textContent = "Ziua frumoasa drag domn/doamna. Bine ati venit pe pagina mea web in aceasta zi superba";
+  } 
+  else {
+    paragraf.textContent = "Seara buna si somn usor draga pasarica <3!";
+  }
+
+});
