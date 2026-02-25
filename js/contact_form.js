907aa175fd7e17d3e15abc4b215c63aeefcaacb7
Tanase Filip Andrei <filip.tanase@student.unitbv.ro>
2026-02-25 17:00:07 +0200

    feat: adauga validare reala la formularul de contact

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,13 +1,40 @@
-function submitForm() {
-    const nume= document.getElementById("name").value;
-    const email=document.getElementById("email").value;
-    const mesaj=document.getElementById("message").value;
-    console.log("Nume:", nume);
-    console.log("Email:", email);
-    console.log("Mesag:",mesaj);
+const form = document.querySelector("form");
 
-    console.warn("Goodbye World!");
-}
+form.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+
+  const nume = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mesaj = document.getElementById("message").value.trim();
+  const feedback = document.getElementById("form-feedback");
+
+  if (nume.length < 2) {
+    feedback.textContent = "Nume prea scurt!";
+    feedback.style.color = "red";
+    return;
+  }
+
+
+  if (!email.includes("@")) {
+    feedback.textContent = "Email invalid!";
+    feedback.style.color = "red";
+    return;
+  }
+
+
+  if (mesaj.length < 10) {
+    feedback.textContent = "Mesajul trebuie să aibă cel puțin 10 caractere!";
+    feedback.style.color = "red";
+    return;
+  }
+
+  feedback.textContent = `Mulțumim, ${nume}! Mesajul a fost trimis.`;
+  feedback.style.color = "green";
+
+  console.log("Formular valid ✅");
+});
+
 window.addEventListener("DOMContentLoaded", function () {
 
   const paragraf = document.querySelector("header p");
