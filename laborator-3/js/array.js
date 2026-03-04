60586e731edb45ea8b5cb8c7df9d5cd120566918
Tanase Filip Andrei <filip.tanase@student.unitbv.ro>
2026-03-04 12:48:37 +0200

    feat: adauga filtre pe array-ul de educatie

--- laborator-3/js/array.js
+++ laborator-3/js/array.js
@@ -4,3 +4,11 @@ const educationArray = Array.from(items).map(function(item) {
     return item.textContent;
 });
 console.log(educationArray);
+const filter2024= educationArray.filter(function(item) {
+    return item.includes("2024");
+});
+console.log("Filtru 2024: ", filter2024);
+const filterUNITBV=educationArray.filter(function(item) {
+    return item.includes("UNITBV");
+});
+console.log("Filtru UNIBV: ", filterUNITBV);
