b2b8db447b728547ae36db95f8a282692d630ab5
Tanase Filip Andrei <filip.tanase@student.unitbv.ro>
2026-03-04 12:51:17 +0200

    feat: extrage primul cuvant din array-ul educatie

--- laborator-3/js/array.js
+++ laborator-3/js/array.js
@@ -4,11 +4,15 @@ const educationArray = Array.from(items).map(function(item) {
     return item.textContent;
 });
 console.log(educationArray);
+const firstWords = educationArray.map(function(item) {
+    return item.split(" ")[0];
+});
+console.log("Primele cuvinte: ", firstWords);
 const filter2024= educationArray.filter(function(item) {
     return item.includes("2024");
 });
 console.log("Filtru 2024: ", filter2024);
-const filterUNITBV=educationArray.filter(function(item) {
-    return item.includes("UNITBV");
+const filterUniversitate=educationArray.filter(function(item) {
+    return item.includes("Universitate");
 });
-console.log("Filtru UNIBV: ", filterUNITBV);
+console.log("Filtru Universitate: ", filterUniversitate);
