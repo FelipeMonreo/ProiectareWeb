82f2693a49b538e13f81b42c322abf47be2517ad
Filip Tanase <ftanase2006@gmail.com>
2026-08-27 16:20:52 +0300

    add Button Sterge component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -11,6 +11,12 @@ function TodoList() {
     setInput('');
   }
 
+  function handleDelete(index) {
+  setTodos(todos.filter(function(_, i) {
+    return i !== index;
+  }));
+}
+
   return (
     <div>
       <h2>Todo List</h2>
@@ -30,6 +36,9 @@ function TodoList() {
           return (
             <li key={index}>
               {todo}
+              <button onClick={() => handleDelete(index)}>
+                Sterge
+              </button>
             </li>
           );
         })}
