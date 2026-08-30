38b7fe692ec9ba1b58f65620d7fef47567e7d6c0
Filip Tanase <ftanase2006@gmail.com>
2026-08-31 02:13:59 +0300

    Add navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './Pages/Home';
 import Projects from './Pages/Projects';
 import Contact from './Pages/Contact';
+import Navbar from './Navbar';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
